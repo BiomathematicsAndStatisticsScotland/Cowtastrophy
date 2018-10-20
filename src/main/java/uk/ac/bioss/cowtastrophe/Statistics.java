3c514a16589e3df1d8e17e61402b69bd27a8b8c7
Anthony O'Hare <ohare.anthony@gmail.com>
2018-10-20 20:15:47 +0100

    Added total cumulative cost of the outbreak.

--- src/main/java/uk/ac/bioss/cowtastrophe/Statistics.java
+++ src/main/java/uk/ac/bioss/cowtastrophe/Statistics.java
@@ -24,8 +24,8 @@ public class Statistics implements Serializable {
         @Override
         public String toString() {
             return String.format("%d, %d, %d, %d, %d, %f", numSusceptibleFarms, numSuspectedFarms,
-                                                       numConfirmedFarms, numCulledFarms,
-                                                       numVaccinatedFarms, cost);
+                                 numConfirmedFarms, numCulledFarms,
+                                 numVaccinatedFarms, cost);
         }
 
         private int numSusceptibleFarms = 0;
@@ -60,7 +60,7 @@ public class Statistics implements Serializable {
         //measurements.numSusceptibleFarms += (getMeasures(prevDay.orElse(0)).numSusceptibleFarms + num);    
         measurements.numSusceptibleFarms += num;
     }
-    
+
     /**
      * Add a number of suspected farms for a given day.
      * @param day the day to make the recording.
@@ -71,8 +71,8 @@ public class Statistics implements Serializable {
 
         measurements.numSuspectedFarms += num;
     }
-    
-        /**
+
+    /**
      * Add a number of vaccinated farms for a given day.
      * @param day the day to make the recording.
      * @param num the number of vaccinated farms on that day.
@@ -91,9 +91,9 @@ public class Statistics implements Serializable {
     public final void addConfirmedFarms(final int day, final int num) {
         Measurements measurements = getMeasures(day);
 
-        measurements.numConfirmedFarms +=  num;
+        measurements.numConfirmedFarms += num;
     }
-    
+
     /**
      * Add a number of culled farms for a given day.
      * @param day the day to make the recording.
@@ -113,27 +113,21 @@ public class Statistics implements Serializable {
     public final void addCost(final int day, final double cost) {
         Measurements measurements = getMeasures(day);
 
-        // cumulative cost thus far.
-//        for ( Map.Entry<Integer, Measurements> entry : stats.entrySet()) {
-//            if (entry.getKey() <= day) {
-//                measurements.cost += entry.getValue().cost;
-//            }
-//        }
         measurements.cost += cost;
     }
-	
-	/** 
-	 * Get cumulative costs at end of given day.
-	 * @param day the day to get the cumulative costs up to.
-	 */
-	public final double getCost(final int day) {
-		Measurements measurements = getMeasures(day);
-		return measurements.cost;
-	}
-    
+
+    /**
+     * Get cumulative costs at end of given day.
+     * @param day the day to get the cumulative costs up to.
+     */
+    public final double getCost(final int day) {
+        Measurements measurements = getMeasures(day);
+        return measurements.cost;
+    }
+
     /**
      * Record a new infection.
-     * @param source the course of the infection.
+     * @param source   the course of the infection.
      * @param infected the newly infected farm.
      */
     public final void addNewInfection(final Farm source, final Farm infected) {
@@ -141,8 +135,7 @@ public class Statistics implements Serializable {
     }
 
     /**
-     * Get the measures for a particular day if they exist or create an empty
-     * measures object if they don't.
+     * Get the measures for a particular day if they exist or create an empty measures object if they don't.
      * @param day the day for which we want the measurements.
      * @return a (guaranteed non-null) Measures object for that day.
      */
@@ -156,17 +149,26 @@ public class Statistics implements Serializable {
     }
 
     /**
-     * Get a string representation of the statistics in the form of a CSV of the time and
-     * measurements.
+     * Get a string representation of the statistics in the form of a CSV of the time and measurements.
      * @return the string.
      */
     @Override
     public final String toString() {
+
         StringBuilder sb = new StringBuilder();
         sb.append("#Time, Susceptible Farms, Farms Suspected, ");
-        sb.append("Farms Confirmed, Farms Culled, Farms Vaccinated, Cost\n");
+        sb.append("Farms Confirmed, Farms Culled, Farms Vaccinated, Cost on day, Total Cost\n");
         stats.entrySet().forEach((entry) -> {
+           
+            double totalCost = 0.0;
+            final int day = entry.getKey();
+            // cumulative cost thus far.
+            totalCost = stats.entrySet().stream().filter((entry2) -> (entry2.getKey() <= day))
+                    .map((entry2) -> entry2.getValue().cost)
+                    .reduce(totalCost, (accumulator, _item) -> accumulator + _item);
+
             sb.append(entry.getKey()).append(", ").append(entry.getValue().toString())
+                    .append(", ").append(totalCost)
                     .append("\n");
         });
         return sb.toString();
@@ -178,5 +180,5 @@ public class Statistics implements Serializable {
     /**
      * The serialVersionUID.
      */
-    private static final long serialVersionUID = 131879930019376504L;
+    private static final long serialVersionUID = 131879930019376509L;
 }
